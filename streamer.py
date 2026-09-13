3e6f8355777cd6120dba39ea7a2f97665cf9afd0
toranaga-sama <odukem622@gmail.com>
2026-09-13 16:51:27 +0300

    some documentation addittions.

--- streamer.py
+++ streamer.py
@@ -4,12 +4,16 @@ import cv2
 filepath=os.path.expanduser("~/sandbox/qr-stream/assets")
 
 def streamer(filepath):
+#this is the function that will initiate the stream, it pops a window and shows the codes.
+#this function still needs a sequencer, thats what will ensure the codes are displayed in order of each other
 
 	filenames=os.listdir(filepath)
 	for f in filenames:
+		#this is the path builder
 		path=filepath+f"/{f}"
 
 		image=cv2.imread(path)
+
 		cv2.imshow("QR stream", image)
 		cv2.waitKey(33)
 
